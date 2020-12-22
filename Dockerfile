cfd2490a279ffc6bb53dc650875428097c1579e4
Abdulazeez Abdulazeez Adeshina <31009679+Youngestdev@users.noreply.github.com>
2020-12-22 05:26:47 +0100

    FastAPI CRUD Application (#1)
    * FastAPI CRUD Application

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.8
+
+ADD requirements.txt /requirements.txt
+
+ADD main.py /main.py
+
+ADD okteto-pipeline.yaml /okteto-pipeline.yaml
+
+ADD okteto-stack.yaml /okteto-stack.yaml
+
+ADD okteto.yml /okteto.yml
+
+RUN pip install -r requirements.txt
+
+EXPOSE 8080
+
+COPY ./app app
+
+CMD ["python3", "main.py"]
---
+++ app/api.py
@@ -0,0 +1,91 @@
+from fastapi import FastAPI, Body
+from fastapi.encoders import jsonable_encoder
+
+from app.model import RecipeSchema
+
+app = FastAPI()
+
+recipes = [
+    {
+        "id": 1,
+        "name": "Donuts",
+        "ingredients": ["Flour", "Milk", "Sugar", "Vegetable Oil"]
+    }
+]
+
+@app.get("/", tags=["Root"])
+def get_root() -> dict:
+    return {
+        "message": "Welcome to the okteto's app."
+    }
+
+@app.get("/recipe", tags=["Recipe"])
+def get_recipes() -> dict:
+    return {
+        "data": recipes
+    }
+
+@app.get("/recipe/{id}", tags=["Recipe"])
+def get_recipe(id: int) -> dict:
+    if id > len(recipes) or id < 1:
+        return {
+            "error": "Invalid ID passed."
+        }
+
+    for recipe in recipes:
+        if recipe['id'] == id:
+            return {
+                "data": [
+                    recipe
+                ]
+            }
+
+    return {
+        "error": "No such recipe with ID {} exist".format(id)
+    }
+
+@app.post("/recipe", tags=["Recipe"])
+def add_recipe(recipe: RecipeSchema = Body(...)) -> dict:
+    recipe.id = len(recipes) + 1
+    recipes.append(recipe.dict())
+    return {
+        "message": "Recipe added successfully."
+    }
+
+@app.put("/recipe", tags=["Recipe"])
+def update_recipe(id: int, recipe_data: RecipeSchema) -> dict:
+    stored_recipe = {}
+    for recipe in recipes:
+        if recipe["id"] == id:
+            stored_recipe = recipe
+        else:
+            return {
+                "error": "No such recipe exist"
+            }
+
+    stored_recipe_model = RecipeSchema(**stored_recipe)
+    update_recipe = recipe_data.dict(exclude_unset=True)
+    updated_recipe = stored_recipe_model.copy(update=update_recipe)
+    recipes[recipes.index(stored_recipe_model)] = jsonable_encoder(updated_recipe)
+
+    return {
+        "message": "Recipe updated successfully."
+    }
+
+@app.delete("/recipe/{id}", tags=["Recipe"])
+def delete_recipe(id: int) -> dict:
+    if id > len(recipes) or id < 1:
+        return {
+            "error": "Invalid ID passed"
+        }
+
+    for recipe in recipes:
+        if recipe['id'] == id:
+            recipes.remove(recipe)
+            return {
+                "data": "Recipe deleted successfully."
+            }
+
+    return {
+        "error": "No such recipe with ID {} exist".format(id)
+    }
